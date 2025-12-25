54383cd45aa5f8e6ba93fdd27b508bbd7b7eb1e9
RealVo <dale@videobooth.ca>
2025-12-25 18:01:43 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import PrivateEnclosedBooth from './pages/PrivateEnclosedBooth';
 import ImplementationProcess from './components/ImplementationProcess';
 import FreeStandingKiosk from './pages/FreeStandingKiosk';
 import DesktopTabletKiosk from './pages/DesktopTabletKiosk';
+import VirtualVideoBooth from './pages/VirtualVideoBooth';
 
 // ------------------------
 // Home / Landing page
@@ -406,6 +407,11 @@ const App: React.FC = () => {
         <Route
           path="/capture/desktop-tablet-kiosk"
           element={<DesktopTabletKiosk />}
+        />
+
+        <Route
+          path="/capture/virtual-video-booth"
+          element={<VirtualVideoBooth />}
         />
       </Routes>
     </Router>
