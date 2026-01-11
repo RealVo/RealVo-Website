26464b7679a4e25d2b67b347f00063387366f763
RealVo <dale@videobooth.ca>
2026-01-11 17:21:43 -0500

    Update UseCases.tsx

--- pages/UseCases.tsx
+++ pages/UseCases.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import UseCaseSpeechBubble from '../components/UseCaseSpeechBubble';
 
 type UseCaseBlock = {
   id: string;
