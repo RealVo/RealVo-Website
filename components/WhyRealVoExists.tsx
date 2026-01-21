990625273010535439ab51b90e5b5bcea6009e2b
RealVo <dale@videobooth.ca>
2026-01-21 16:18:08 -0500

    Update WhyRealVoExists.tsx

--- components/WhyRealVoExists.tsx
+++ components/WhyRealVoExists.tsx
@@ -128,7 +128,7 @@ const WhyRealVoExists: React.FC = () => {
             </div>
 
             {/* ✅ Transition Insight Card (LOCKED) */}
-            <div className="mt-20 md:mt-24 flex justify-center">
+            <div className="mt-28 md:mt-32 flex justify-center">
               <div
                 className="
                   relative
