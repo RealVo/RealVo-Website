654ff5906a52d4aca9a3c6d53d1713cfa6c18450
RealVo <dale@videobooth.ca>
2026-01-22 19:42:05 -0500

    Update WhyWhatBridge.tsx

--- components/WhyWhatBridge.tsx
+++ components/WhyWhatBridge.tsx
@@ -7,7 +7,7 @@ const WhyWhatBridge: React.FC = () => {
   return (
     <div className="scroll-mt-24">
       {/* WHY (white) + seam overlap */}
-      <div className="relative pb-14 sm:pb-16 md:pb-16 lg:pb-14">
+      <div className="relative pb-14 sm:pb-16 md:pb-14 lg:pb-12">
   <WhyRealVoExists showInsightCard={false} />
 
         {/* Seam Insight Card overlaps down into the next section */}
