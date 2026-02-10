748d7e653ebe6dc6f5f7f44faaecf8fad51b3fe8
RealVo <dale@videobooth.ca>
2026-02-09 20:49:24 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -28,6 +28,7 @@ import DesktopTabletKiosk from './pages/DesktopTabletKiosk';
 import VirtualVideoBooth from './pages/VirtualVideoBooth';
 import ContactForm from './components/ContactForm';
 import VBPlatform_More from './pages/VBPlatform_More';
+import TorontoVideoBooth from './pages/TorontoVideoBooth';
 
 // ------------------------
 // Home / Landing page
@@ -214,6 +215,7 @@ const App: React.FC = () => {
         <Route path="/capture/desktop-tablet-kiosk" element={<DesktopTabletKiosk />} />
         <Route path="/capture/virtual-video-booth" element={<VirtualVideoBooth />} />
         <Route path="/vbplatform-more" element={<VBPlatform_More />} />
+        <Route path="/toronto-video-booth" element={<TorontoVideoBooth />} />
       </Routes>
     </Router>
   );
