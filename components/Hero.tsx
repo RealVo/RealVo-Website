f988ba8b956e5f813f5d12c3bf9235f421040fe2
RealVo <dale@videobooth.ca>
2026-02-12 19:52:52 -0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -97,7 +97,7 @@ const Hero: React.FC = () => {
   };
 
   return (
-  <Section className="!pt-6 pb-12 md:pt-14 md:pb-20 relative overflow-visible md:overflow-hidden">
+  <Section className="pt-12 pb-12 md:!pt-14 md:pb-20 relative overflow-hidden">
     <div
       ref={heroRef}
       className={`
