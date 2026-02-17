0af1c077801acfdc36e1e47a43900665e9bd82ab
RealVo <dale@videobooth.ca>
2026-02-17 16:59:30 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer className="bg-realvo-charcoal text-white py-12 border-t border-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Top Row */}
-        <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between gap-10">
+        <div className="flex flex-col lg:flex-row lg:items-end lg:justify-between gap-10">
           {/* Left: Logo + Description */}
           <div className="max-w-sm">
             <a href="/">
