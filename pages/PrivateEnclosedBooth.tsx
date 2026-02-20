791140da915ec69a7e7dd08e05feac0d43521d6f
RealVo <dale@videobooth.ca>
2026-02-20 13:35:38 -0500

    Update PrivateEnclosedBooth.tsx

--- pages/PrivateEnclosedBooth.tsx
+++ pages/PrivateEnclosedBooth.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const VISIBLE_DESKTOP_IMAGES = 4;
