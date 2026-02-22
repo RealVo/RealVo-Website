95b0f04670dffb5e912bbbc2a65bff86ee45021f
RealVo <dale@videobooth.ca>
2026-02-22 10:47:04 -0500

    Update Contact.tsx

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactForm from '../components/ContactForm';
 
   const Contact: React.FC = () => {
