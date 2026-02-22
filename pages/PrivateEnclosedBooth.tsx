bddabe4718c9fe9d874cadd6ffcb5ceddfda3b86
RealVo <dale@videobooth.ca>
2026-02-21 21:47:28 -0500

    Update PrivateEnclosedBooth.tsx

--- pages/PrivateEnclosedBooth.tsx
+++ pages/PrivateEnclosedBooth.tsx
@@ -451,7 +451,7 @@ const PrivateEnclosedBooth: React.FC = () => {
 
               <button
                 type="button"
-                onClick={() => scrollToSectionGlobal('contact')}
+                onClick={() => scrollToSectionGlobal('/contact')}
                 className="inline-flex items-center justify-center rounded-full px-6 py-3 text-base font-medium bg-realvo-teal text-white hover:bg-realvo-teal/90 transition whitespace-nowrap md:px-7 md:py-3.5"
               >
                 Let&apos;s get started
